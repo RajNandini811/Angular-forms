8f9dd3d3a375c2729241979de34b1eb238c7257d
RajNandini811 <rajkop21@gmail.com>
2024-07-13 13:14:35 +0530

    Test comit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  ngOnInit(){
+    console.log("Inside login component");
+  }
 }
