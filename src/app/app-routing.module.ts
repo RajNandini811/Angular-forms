d80e8d5499ac97bca2d5b6407fc1ee553d2c2c18
RajNandini811 <rajkop21@gmail.com>
2024-07-13 13:05:05 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',redirectTo: 'login',pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'headh', component: HeaderComponent },
+  { path: 'movie', component: MovieComponent },
+  { path: '**', component: LoginComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
